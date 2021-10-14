327ab3d3e3bc1b8b13f29d865e26395cb323edbf
alfanhib <38372071+alfanhib@users.noreply.github.com>
2021-10-14 17:51:42 +0700

    Update script-json.js

--- script-json.js
+++ script-json.js
@@ -12,12 +12,14 @@ const getListMhs = () => {
             ourProduct.innerHTML = "";
             let pd = responseJson.product;
             pd.forEach(item => {
+                let image = item.foto[0];
+                consolt.log("isi image", image)
                 ourProduct.innerHTML += `
                   
                    <div class="col-md-6 mt-5 mb-5">
 
                     <a href="detail-product.html?id=${item.id}"> 
-                        <img src="${item.foto[0}" width="90%">
+                        <img src="${image}" width="90%">
                         <div class="des-product">
                         <h2 class="mt-3">${item.nama} </h2>
                         <h6 class="mt-3">${item.harga} </h6>
